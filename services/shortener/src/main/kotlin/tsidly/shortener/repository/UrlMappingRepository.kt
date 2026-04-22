336c4a79c6296db4a636e49a2f7560af6c61a533
DEOKJAE KWON <juintin@kakao.com>
2026-04-22 20:35:25 +0900

    refactor: shortenUrl 로직 수정

--- services/shortener/src/main/kotlin/tsidly/shortener/repository/UrlMappingRepository.kt
+++ services/shortener/src/main/kotlin/tsidly/shortener/repository/UrlMappingRepository.kt
@@ -3,4 +3,6 @@ package tsidly.shortener.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import tsidly.shortener.entity.UrlMapping
 
-interface UrlMappingRepository : JpaRepository<UrlMapping, String>
+interface UrlMappingRepository : JpaRepository<UrlMapping, String> {
+    fun findByOriginalUrl(originalUrl: String): UrlMapping?
+}
