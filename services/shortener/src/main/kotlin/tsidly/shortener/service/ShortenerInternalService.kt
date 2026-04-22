34011ab4bfc17cb9a2d7968fc4984b93e9abf841
DEOKJAE KWON <juintin@kakao.com>
2026-04-22 19:13:03 +0900

    refactor: Redis 캐시 저장 로직 추가

--- services/shortener/src/main/kotlin/tsidly/shortener/service/ShortenerInternalService.kt
+++ services/shortener/src/main/kotlin/tsidly/shortener/service/ShortenerInternalService.kt
@@ -4,11 +4,14 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import tsidly.shortener.dto.response.UrlMappingResponse
+import tsidly.shortener.infrastructure.redis.UrlMappingRedisWriter
 import tsidly.shortener.repository.UrlMappingRepository
+import java.time.Duration
 
 @Service
 class ShortenerInternalService(
     private val urlMappingRepository: UrlMappingRepository,
+    private val urlMappingRedisWriter: UrlMappingRedisWriter,
 ) {
 
     @Transactional(readOnly = true)
@@ -17,6 +20,11 @@ class ShortenerInternalService(
     ): UrlMappingResponse {
         val urlMapping = urlMappingRepository.findByIdOrNull(shortId)
             ?: throw NoSuchElementException("존재하지 않는 단축 URL입니다.")
+
+        urlMappingRedisWriter.write(
+            urlMapping = urlMapping,
+            ttl = Duration.ofHours(1),
+        )
 
         return UrlMappingResponse.from(urlMapping)
     }
